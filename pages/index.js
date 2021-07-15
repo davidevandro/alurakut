803264bae64bbb9e70124c9c0c83c9f31c4cd30b
David Martins <davidevandro@gmail.com>
2021-07-15 11:58:34 -0300

    [D1.1]  Modificar valores legal, sexy e confiavel

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
               Bem vindo(a)
             </h1>
 
-            <OrkutNostalgicIconSet/>
+            <OrkutNostalgicIconSet confiavel = "3" legal = "3" sexy = "3"/>
           </Box>
         </div>
         <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
